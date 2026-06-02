f38ada9a5534060ca5bd044281790e2ff57b9b8a
MohamedYashar <arrafathyas@gmail.com>
2026-06-02 11:53:31 +0530

    pop up menu added for delete button

--- src/movies/MovieList.jsx
+++ src/movies/MovieList.jsx
@@ -6,10 +6,20 @@ import EditIcon from "@mui/icons-material/Edit";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min.js";
 import { useEffect, useState } from "react";
 
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+
 function MovieList() {
   const history = useHistory();
 
   const [movie, setMovie] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
 
   const getMovies = () => {
     fetch(`https://6a1bf1008858a003817b5635.mockapi.io/movies/`, {
@@ -21,18 +31,40 @@ function MovieList() {
 
   useEffect(getMovies, []);
 
-  const removeMovie = (id) => {
-    console.log("Deleting Movie", id);
-    // remove movie logic with filter is simple
-    // const removeMovieIndex = index;
-    // const remainingMovies = movie.filter((mv, idx) => idx != removeMovieIndex);
-    // console.log(movie, remainingMovies, index);
-    // // and update it
-    // setMovie(remainingMovies);
-    // after deleting you have to refresh
-    fetch(`https://6a1bf1008858a003817b5635.mockapi.io/movies/${id}`, {
-      method: "DELETE",
-    }).then(() => getMovies());
+  // const removeMovie = (id) => {
+  //   console.log("Deleting Movie", id);
+  //   // remove movie logic with filter is simple
+  //   // const removeMovieIndex = index;
+  //   // const remainingMovies = movie.filter((mv, idx) => idx != removeMovieIndex);
+  //   // console.log(movie, remainingMovies, index);
+  //   // // and update it
+  //   // setMovie(remainingMovies);
+  //   // after deleting you have to refresh
+  //   fetch(`https://6a1bf1008858a003817b5635.mockapi.io/movies/${id}`, {
+  //     method: "DELETE",
+  //   }).then(() => getMovies());
+  // };
+
+  const handleDeleteClick = (id) => {
+    setSelectedMovieId(id);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedMovieId(null);
+  };
+
+  const handleConfirmDelete = () => {
+    fetch(
+      `https://6a1bf1008858a003817b5635.mockapi.io/movies/${selectedMovieId}`,
+      {
+        method: "DELETE",
+      },
+    ).then(() => {
+      getMovies();
+      handleClose();
+    });
   };
 
   return (
@@ -62,15 +94,47 @@ function MovieList() {
               aria-label="delete"
               color="error"
               size="large"
+              // onClick={() => {
+              //   removeMovie(id);
+              // }} // remove movie logic with filter is simple
+
               onClick={() => {
-                removeMovie(id);
-              }} // remove movie logic with filter is simple
+                handleDeleteClick(id);
+              }}
             >
               <DeleteIcon />
             </IconButton>
           }
         />
       ))}
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Delete Movie?</DialogTitle>
+
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Delete this movie permanently? This action cannot be undone.
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+
+          <Button
+            color="error"
+            variant="contained"
+            onClick={handleConfirmDelete}
+            autoFocus
+          >
+            Yes, Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </section>
   );
 }
